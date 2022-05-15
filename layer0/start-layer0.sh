c518af2845ead902529d8813b1fc45c26f62ea23
FrancoisSevestre <francois.sevestre@live.fr>
2022-05-15 23:19:38 +0200

    misc fix

--- layer0/start-layer0.sh
+++ layer0/start-layer0.sh
@@ -8,11 +8,11 @@ source /install/layer0/functions.sh
 
 # System parameters
 log -c "ln -sf /usr/share/zoneinfo/$zone_info /etc/localtime" -m "Defining timezone" -l "LAYER0"
-log -c "echo $locale >> /etc/locale.gen" -m "Changing locale settings" -l "LAYER0"
+log -c "echo $locale" -f "/etc/locale.gen" -m "Changing locale settings" -l "LAYER0"
 log -c "locale-gen" -m "Generating locale configuration" -l "LAYER0"
-log -c "echo LANG=$language > /etc/locale.conf" -m "Setting language" -l "LAYER0"
-log -c "echo KEYMAP=$vconsole > /etc/vconsole.conf" -m "Setting Keyboard configuration" -l "LAYER0"
-log -c "echo $hostname > /etc/hostname" -m "Setting hostname" -l "LAYER0"
+log -c "echo LANG=$language" -f "/etc/locale.conf" -m "Setting language" -l "LAYER0"
+log -c "echo KEYMAP=$vconsole" -f "/etc/vconsole.conf" -m "Setting Keyboard configuration" -l "LAYER0"
+log -c "echo $hostname" -f "/etc/hostname" -m "Setting hostname" -l "LAYER0"
 
 # Installing tmux
 log -c "pacman -S tmux expect --noconfirm" -m "Installing misc tools" -l "LAYER0"
